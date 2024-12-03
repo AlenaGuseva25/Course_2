f9393730dae056f5d6bc69fa4768584f403a52b6
Alena <alittlekindness94@gmail.com>
2024-12-03 14:37:29 +0500

    абстрактный для json

--- src/abstract_classes.py
+++ src/abstract_classes.py
@@ -18,6 +18,18 @@ class JobAPI(ABC):
 
 class VacancyStorage(ABC):
     """Абстрактный класс для работы с JSON """
+
     @abstractmethod
     def add_vacancy(self, vacancy):
+        """Метод добавления в файл вакансий"""
+        pass
+
+    @abstractmethod
+    def get_vacancies(self):
+        """Метод получения данных из файла"""
+        pass
+
+    @abstractmethod
+    def remove_vacancy(self, vacancy_name):
+        """Метод удаления вакансий"""
         pass
